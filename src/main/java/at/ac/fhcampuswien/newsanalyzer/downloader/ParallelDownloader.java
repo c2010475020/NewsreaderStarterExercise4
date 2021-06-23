9873d54326855091dc1983a930639785c2fd7988
Mario Ikic <71633522+c2010475020@users.noreply.github.com>
2021-06-23 11:21:23 +0200

    Changes

--- src/main/java/at/ac/fhcampuswien/newsanalyzer/downloader/ParallelDownloader.java
+++ src/main/java/at/ac/fhcampuswien/newsanalyzer/downloader/ParallelDownloader.java
@@ -1,29 +1,30 @@
 package at.ac.fhcampuswien.newsanalyzer.downloader;
 
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
-public class ParallelDownloader extends Downloader {
+public class ParallelDownloader extends Downloader{
 
-    ExecutorService executorService = Executors.newFixedThreadPool(10);
+    private int numWorkers = Runtime.getRuntime().availableProcessors();
+    ExecutorService pool = Executors.newFixedThreadPool(numWorkers);
 
     @Override
-    public int process(List<String> urls) {
+    public int process(List<String> urls) throws ExecutionException, InterruptedException {
+        long startTime = System.currentTimeMillis();
         int count = 0;
-        for (String url : urls) {
-            try {
-                Future<?> taskStatus = executorService.submit(() -> {
-                    System.out.printf("Starting Thread %s", Thread.currentThread().getName());
-                    saveUrl2File(url);
-                });
-                count ++;
-            } catch (Exception e) {
-                System.out.println("Error");
-            }
+        for(String url : urls) {
+            Future<String> future = pool.submit( () -> saveUrl2File(url));
+            if(future.get() != null) count++;
         }
-        executorService.shutdown();
+        long endTime = System.currentTimeMillis();
+
+        long timeElapsed = endTime - startTime;
+
+        System.out.println("Execution time in milliseconds at parallel download: " + timeElapsed);
+
         return count;
     }
 }
